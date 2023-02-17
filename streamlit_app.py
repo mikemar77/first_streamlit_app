b5c189422074d97a9d3051d3819c662d37491752
mikemar77 <124356446+mikemar77@users.noreply.github.com>
2023-02-17 23:31:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,13 +40,10 @@ try:
     else:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-
 except URLError as e:
      streamlit.error()
-
 #don't run anything past here while we troubleshoot
 streamlit.stop()
-
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
